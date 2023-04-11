dd5f04839a70ca909fc091bb07596317ddfd41cc
kaatenn <komeijikaatenn@gmail.com>
2023-04-11 13:02:44 +0800

    add comment

--- Tracer/Scene_Object.cs
+++ Tracer/Scene_Object.cs
@@ -2,14 +2,20 @@
 
 namespace _3D_viewer.Tracer;
 
+/// <summary>
+///     Represents a object in the scene.
+/// </summary>
 public class Scene_Object
 {
-    private List<Triangle> _triangles;
+    private List<Triangle> _triangles; // The list of triangles in the object.
 
-    public Bounding_Volume bounding_volume; 
+    public Bounding_Volume bounding_volume; // The bounding volume of the object.
 
-    private double refractive_index { get; set; }
+    private double refractive_index { get; set; } // the refractive index of the object.
     
+    /// <summary>
+    /// Constructor to create an instance of the default Scene_Object class.
+    /// </summary>
     public Scene_Object()
     {
         _triangles = new List<Triangle>();
--- Tracer/Triangle.cs
+++ Tracer/Triangle.cs
@@ -2,12 +2,19 @@
 
 namespace _3D_viewer.Tracer;
 
+/// <summary>
+///     Represents a triangle
+/// </summary>
 public class Triangle
 {
-    public Point3D[] vertices { get; set; }
-    private readonly Vector3D[] edge = new Vector3D[3];
-    private Surface surface;
+    public Point3D[] vertices { get; set; } // Three vertices of the triangle
+    private readonly Vector3D[] edge = new Vector3D[3]; // Three edges of the triangle
+    private Surface surface; // The surface which the triangle belongs to
 
+    /// <summary>
+    /// Constructor to create a new instance of the Triangle class.
+    /// </summary>
+    /// <param name="vertices">The three vertices of the triangle</param>
     public Triangle(Point3D[] vertices)
     {
         this.vertices = vertices;
